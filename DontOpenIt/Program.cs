8044a7111d43f11b16dfe10ac36cb3d40ea94f28
yutoVR <yuto.fushimi@icloud.com>
2021-06-06 03:37:28 +0900

    Revamped to use ProcessWatcher.

--- DontOpenIt/Program.cs
+++ DontOpenIt/Program.cs
@@ -1,102 +1,76 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Globalization;
-using System.IO;
 using System.Windows.Forms;
+using ProcessWatcher;
 
 namespace DontOpenIt
 {
     internal class Program
     {
-        class Parameters
+        enum TimeFrame
         {
-            public Parameters(string path, string name, int begin, int end)
-            {
-                this.path = path;
-                this.name = name;
-                this.begin = begin;
-                this.end = end;
-            }
-
-            public string path;
-            public string name;
-            public int begin;
-            public int end;
+            Before,
+            Working,
+            After
         }
 
         public static void Main(string[] args)
         {
-            var (success, p) = TryParseArgs(args);
-            if (!success) return;
+            var watchdog = new Watchdog();
+            watchdog.Attach(OnProcessCreated, out _);
+            watchdog.Run();
+            Console.ReadLine();
+        }
 
+        static void OnProcessCreated(Process process)
+        {
+            Console.WriteLine(process.ProcessName);
+            if (process.ProcessName == "Slack")
+            {
+                var timeFrame = GetTimeFrame();
+                if (timeFrame != TimeFrame.Working)
+                {
+                    var yes = ConfirmOpen(timeFrame, process.ProcessName);
+                    if (!yes) process.Kill();
+                }
+            }
+        }
+
+        static TimeFrame GetTimeFrame()
+        {
             var now = DateTime.Now.TimeOfDay;
 
-            var timeMessage = "";
-            if (TimeSpan.FromHours(4) <= now && now < TimeSpan.FromHours(p.begin))
+            if (TimeSpan.FromHours(4) <= now && now < TimeSpan.FromHours(9))
             {
-                timeMessage = $"{p.begin}時より早いです。";
-            }
-            else if (TimeSpan.FromHours(4) > now || now > TimeSpan.FromHours(p.end))
-            {
-                timeMessage = $"{p.end}時を過ぎています。";
+                return TimeFrame.Before;
             }
 
-            if (string.IsNullOrEmpty(timeMessage))
+            if (TimeSpan.FromHours(4) > now || now > TimeSpan.FromHours(20))
             {
-                Process.Start(p.path);
+                return TimeFrame.After;
             }
-            else
-            {
-                var yes = Confirm(timeMessage, p.name);
-                if (yes) Process.Start(p.path);
-            }
+
+            return TimeFrame.Working;
         }
 
-        static (bool success, Parameters parameters) TryParseArgs(string[] args)
+        static bool ConfirmOpen(TimeFrame timeFrame, string name)
         {
-            if (args.Length != 3)
+            var message = "";
+            switch (timeFrame)
             {
-                MessageBox.Show("引数の数が誤っています。\n1:起動するプログラムのパス\n2:警告の開始時間（24時間表記）\n3:警告の終了時間（24時間表記）");
-                return (false, null);
+                case TimeFrame.Before:
+                    message = "9時になっていませんがよろしいですか？";
+                    break;
+
+                case TimeFrame.After:
+                    message = "20時を過ぎていますがよろしいですか？";
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(timeFrame), timeFrame, null);
             }
 
-            var path = args[0];
-            var name = "";
-            if (path.EndsWith(":"))
-            {
-                var textInfo = CultureInfo.CurrentCulture.TextInfo;
-                name = textInfo.ToTitleCase(path).Replace(":", "");
-            }
-            else if (!path.Contains("\\") && path.EndsWith("exe"))
-            {
-                var textInfo = CultureInfo.CurrentCulture.TextInfo;
-                name = Path.GetFileNameWithoutExtension(textInfo.ToTitleCase(path));
-            }
-            else if (path.Contains("\\") && path.EndsWith("exe"))
-            {
-                name = Path.GetFileNameWithoutExtension(path);
-            }
-            else
-            {
-                MessageBox.Show($"指定されたパスは利用できません。\n{path}");
-                return (false, null);
-            }
-
-            var beginParsed = int.TryParse(args[1], out var begin);
-            var endParsed = int.TryParse(args[2], out var end);
-
-            if (!beginParsed || !endParsed)
-            {
-                MessageBox.Show($"時刻指定を読み取れませんでした。\n{begin}\n{end}");
-                return (false, null);
-            }
-
-            return (true, new Parameters(path, name, begin, end));
-        }
-
-        static bool Confirm(string timeMessage, string name)
-        {
-            var result = MessageBox.Show($"{timeMessage}本当に {name} を開いて良いですか？", "Don't Open It", MessageBoxButtons.YesNo);
+            var result = MessageBox.Show($"{name} を開きましたね？ {message}", "Don't Open It", MessageBoxButtons.YesNo);
             if (result == DialogResult.No) return false;
             result = MessageBox.Show("本当に良いんですね？ 他にやることはないの？", "Don't Open It!!!", MessageBoxButtons.YesNo);
             if (result == DialogResult.No) return false;
