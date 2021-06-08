11aa2de84617e33e2df24c84266c3efa6b29a0cd
yutoVR <yuto.fushimi@icloud.com>
2021-06-09 03:27:56 +0900

    wip: Porting to WPF.

---
+++ DontOpenItWPF/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+
+namespace DontOpenItWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        [DllImport("user32.dll")]
+        static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SendMessage(IntPtr hwnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+        const int GWL_STYLE = -16;
+        const int WS_MAXIMIZEBOX = 0x10000;
+        const int WS_MINIMIZEBOX = 0x20000;
+        const int WM_SETICON = 0x0080;
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            var handle = new WindowInteropHelper(this).Handle;
+            var style = GetWindowLong(handle, GWL_STYLE);
+            style &= ~WS_MAXIMIZEBOX & ~WS_MINIMIZEBOX;
+            SetWindowLong(handle, GWL_STYLE, style);
+            SetWindowLong(handle, WM_SETICON, 0);
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
